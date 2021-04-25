f3ec3c1f03c802112445f705d3258e8ec611ccdc
Emre Karaman <karaman.66@hotmail.com>
2021-04-25 22:25:56 +0300

    Add project files.

---
+++ IdentityKullaniciYonetimi/Models/ApplicationUser.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityKullaniciYonetimi.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+       
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public int UsernameChangeLimit { get; set; } = 10;
+        public byte[] ProfilePicture { get; set; }
+    }
+}
